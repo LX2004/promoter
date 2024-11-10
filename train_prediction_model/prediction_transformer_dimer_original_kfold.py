3c2dee847f72bd475f9f2a25a659e14a23a2429d
LX2004 <77057762+LX2004@users.noreply.github.com>
2024-11-10 15:18:50 +0800

    Update prediction_transformer_dimer_original_kfold.py

--- train_prediction_model/prediction_transformer_dimer_original_kfold.py
+++ train_prediction_model/prediction_transformer_dimer_original_kfold.py
@@ -30,6 +30,63 @@ class CustomDataset(Dataset):
         label = self.labels[idx]
         return feature, label
 
+def make_dataset_for_prediction_prometer(promoter, strength, seq_length=50 ):
+
+    folder_path = 'dataset_seq_length={0}/'.format(seq_length)
+
+    if not os.path.exists(folder_path):
+
+        os.makedirs(folder_path)
+        print(f"The folder '{folder_path}' has been created.")
+
+    else:
+        print(f"The folder '{folder_path}' already exists.")
+
+    strength = np.array(strength)
+
+    length = len(promoter[0])
+    print(strength[0])
+    print('length = ',length)
+
+    strength_float = strength.astype(float)
+    strength_float = np.log10(strength_float)
+
+    strength_max = np.max(strength_float)
+    strength_min = np.min(strength_float)
+
+    print('max_strength = ',strength_max)
+    print('min_strength = ',strength_min)
+
+    number = 0
+    features = []
+    labels = []
+
+    for sequence, score in zip(promoter, strength_float):
+
+        if  50 != len(sequence) or score == 0.0:
+
+            print('the length of sequence is ',len(sequence))
+            print('promoter = ',sequence.upper())
+            print('strength = ',score)
+            continue
+
+        feature = Dimer_split_seqs(sequence[-seq_length:]) 
+        feature = np.array(feature)
+        feature = feature.astype(int)
+
+        label = (score -  strength_min)/( strength_max -   strength_min)
+        labels.append(label)
+        features.append(feature)
+        
+        # sample = Seq_sample(feature = feature, label = label)
+
+        # with open('./dataset_seq_length={0}/sample_{2}.pkl'.format(seq_length, number), 'wb') as file:
+        #     pickle.dump(sample, file)
+
+        number += 1
+    print('number = ',number)
+
+
 def train(params, features_array, labels_array):
     
     print('params = ',params)
