23cecfc6a0c113cb40eb0a41b9b6c3294008e0db
LX2004 <77057762+LX2004@users.noreply.github.com>
2024-11-10 15:11:19 +0800

    Update prediction_transformer_dimer_original_kfold.py

--- train_prediction_model/prediction_transformer_dimer_original_kfold.py
+++ train_prediction_model/prediction_transformer_dimer_original_kfold.py
@@ -191,9 +191,8 @@ if __name__ == '__main__':
        }
 
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    torch.cuda.set_device(params['device_num'])
-    print('device =',device)
+    device = torch.device(f'cuda:{params["device_num"]}' if torch.cuda.is_available() else 'cpu')
+    print('device =', device)
 
     space = {
 
