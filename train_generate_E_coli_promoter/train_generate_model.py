f245a88016040bbebfab17ab7d2356d7aeb586c4
LX2004 <77057762+LX2004@users.noreply.github.com>
2024-11-10 23:12:00 +0800

    Update train_generate_model.py

--- train_generate_E_coli_promoter/train_generate_model.py
+++ train_generate_E_coli_promoter/train_generate_model.py
@@ -162,7 +162,10 @@ def main():
             if iteration % args.checkpoint_rate == 0:
 
                 model_filename = f"{args.log_dir}/{args.project_name}-{args.run_name}-iteration-{iteration}--kernel={1+2*args.out_init_conv_padding}--model.pth"
+                optimizer_filename = f"{args.log_dir}/{args.project_name}-{args.run_name}-iteration-{iteration}--kernel={1+2*args.out_init_conv_padding}--optimizer.pth"
+                
                 torch.save(diffusion.state_dict(), model_filename)
+                torch.save(optimizer.state_dict(), optimizer_filename)
 
     except KeyboardInterrupt:
 
