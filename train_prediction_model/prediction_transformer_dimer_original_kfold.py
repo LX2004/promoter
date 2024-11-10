a5020def519110d211604104ba460464cc1f03e2
LX2004 <77057762+LX2004@users.noreply.github.com>
2024-11-10 18:46:52 +0800

    Update prediction_transformer_dimer_original_kfold.py

--- train_prediction_model/prediction_transformer_dimer_original_kfold.py
+++ train_prediction_model/prediction_transformer_dimer_original_kfold.py
@@ -32,15 +32,15 @@ class CustomDataset(Dataset):
 
 def make_dataset_for_prediction_prometer(promoter, strength, seq_length=50 ):
 
-    folder_path = 'dataset_seq_length={0}/'.format(seq_length)
+    # folder_path = 'dataset_seq_length={0}/'.format(seq_length)
 
-    if not os.path.exists(folder_path):
+    # if not os.path.exists(folder_path):
 
-        os.makedirs(folder_path)
-        print(f"The folder '{folder_path}' has been created.")
+    #     os.makedirs(folder_path)
+    #     print(f"The folder '{folder_path}' has been created.")
 
-    else:
-        print(f"The folder '{folder_path}' already exists.")
+    # else:
+    #     print(f"The folder '{folder_path}' already exists.")
 
     strength = np.array(strength)
 
